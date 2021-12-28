266b1ae843ef086e477e9e6bfe426646053844f1
Camille1905 <bourcamille00@gmail.com>
2021-12-28 15:54:40 +0100

    Update ping controller to status controller.

--- src/main/java/fr/camillebour/covidapp/controllers/StatusController.java
+++ src/main/java/fr/camillebour/covidapp/controllers/StatusController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController {
+public class StatusController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    @GetMapping("/status")
+    public String status() {
+        return "up";
     }
 }
